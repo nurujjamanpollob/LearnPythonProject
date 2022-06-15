b6c990202e8a37576e409ab41c90ce027f005e54
Nurujjaman Pollob <nurujjamanpollob@gmail.com>
2022-06-15 11:38:20 +0600

    updated example

--- Lists.py
+++ Lists.py
@@ -4,3 +4,17 @@ basicList = ["Hi", "this is", "python", "language"]
 # Print the basicList
 # Use str(object) method to convert object to String
 print("The basic list: " + str(basicList))
+
+# Create a list of mixed data types in a single list
+mixedList = ["Hi", "I am number", 10, "in my college"]
+
+# Print the mixedList
+# Use str(object) method to convert object to String
+print("The mixed list: " + str(mixedList))
+
+# Create a nested list
+nestedList = ["Hi", "I gonna create a", "nested list now -> ", ["I", "am", "nested", "list", "😁"]]
+
+# Print the mixedList
+# Use str(object) method to convert object to String
+print("The nested list: " + str(nestedList))
