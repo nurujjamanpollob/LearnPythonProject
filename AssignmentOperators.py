e6c006a3617b22fdde682722e1573d0d198fa60d
Nurujjaman Pollob <nurujjamanpollob@gmail.com>
2022-06-19 09:53:20 +0600

    Assignment Operator ref updated

--- AssignmentOperators.py
+++ AssignmentOperators.py
@@ -212,6 +212,57 @@ var >>= 2
 # Print
 print("right shift 2 bit from 5 binary bit produced output -> %d" % var)
 
-
-
-
+"""
+    Matrix multiplication example.
+    
+    This example going to cover matrix multiplication by a number or or by another matrix.
+    
+    This example gonna cover two basic and simplest example:
+    
+    Example one -> Matrix m where it has two row and three col like below, and multiply this matrix with 2 will result:
+    
+                                                                      |     | 2, 5, 3 |
+                                                                  m = | 2 * | 4, 6, 1 |
+                                                                      |     | 3, 6, 2 |
+                                                                      _________________
+    Expressions 1 row and col -> 2 * 2 = 4, 2 * 5 = 10, 2 * 3 = 6 so ->     | 4, 10, 6 |
+    Expressions 2 row and col -> 2 * 4 = 8, 2 * 6 = 12, 2 * 1 = 2 so ->     | 8, 12, 2 | = updatedMatrix
+    Expressions last row and col -> 2 * 3 = 6, 2 * 6 = 12, 2 * 2 = 4 ->     | 6, 12, 4 |  
+                                                                           
+    So, you see, it's easy to do matrix multiplication with a single number and a matrix.
+    
+    
+    Example two -> Matrix m and Matrix n, which is has same 3 row and 2 col, like below, we need to do dot product,
+    For example, follow this example: 
+    
+    
+                                               | 2, 4 |     | 4, 5 |       
+                                         m =   |      |  *  |      | = n
+                                               | 4, 2 |     | 6, 3 | 
+                                               
+    There are possible four expression, that are given below:
+    
+    Expression one -> (2, 4) . (4, 6) = (2 * 4) + (4 * 6) = 8 + 24
+                                                          =   32 so -> | 32, * |
+                                                                       | *,  * |
+                                                                    
+    Expression two -> (2, 4) . (5, 3) = (2 * 5) + (4 * 3) = 10 + 12
+                                                          = 22 so -> | 32, 22 |
+                                                                     | *,   * |
+                                                                     
+    Expression three -> (4, 2) . (4, 6) = (4 * 4) + (2 * 6) = 16 + 12
+                                                            = 28 so -> | 32, 22 |
+                                                                       | 28,  * |
+                                                                       
+    Last expression -> (4, 2) . (5, 3) = (4 * 5) + (2 * 3) = 20 + 6
+                                                           = 26 so -> | 32, 22 |
+                                                                      | 28, 26 |
+                                                                      
+                                        | 32, 22 |
+    So, the final output should be ->   |        |                    
+                                        | 28, 26 |
+                                        
+    
+    This usages mathematics function 'matmul' -> '@=' assigment example. The example will be updated later
+                              
+"""
