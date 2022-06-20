a8b8f9633af7baef07ffe67497646d1af018de8a
Nurujjaman Pollob <nurujjamanpollob@gmail.com>
2022-06-20 19:03:01 +0600

    new class added, will update shortly

---
+++ Dictionaries.py
@@ -0,0 +1,6 @@
+"""
+    Python dictionary - a dictionary is a data type like arrays, but it stores data in key value format
+
+    This example going to show you how to create a dictionary, add key value pair, query and update it
+
+"""
